efa25b57bbc4340c11219061cbf2c728cd36392e
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2021-08-27 17:45:12 +0530

    db

---
+++ DemoDBProject/dbo/Tables/EmployeeDetails.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[EmployeeDetails] (
+    [EmployeeId]   INT          IDENTITY (1, 1) NOT NULL,
+    [EmployeeName] VARCHAR (50) NULL,
+    CONSTRAINT [PK_EmployeeDetails] PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
